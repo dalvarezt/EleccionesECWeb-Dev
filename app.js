e85ab845f51b10c188079e27b7baa85697a0be9a
Daniel Alvarez Torres <dalvarezt@outlook.com>
2017-02-04 09:30:04 -0500

    Cambio de query para que use vista

--- app.js
+++ app.js
@@ -28,67 +28,62 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 	console.log("server starting on " + appEnv.url);
 });
 
-var testValue={"Bucaram":[
-		{"label":"Positivo","data":22,"color":"#0eea57"},
-		{"label":"Neutral","data":12,"color":"#666666"},
-		{"label":"Negativo","data":80,"color":"#f44242"}],
-	"Lasso":[
-		{"label":"Positivo","data":225,"color":"#0eea57"},
-		{"label":"Neutral","data":168,"color":"#666666"},
-		{"label":"Negativo","data":109,"color":"#f44242"}],
-	"Moncayo":[
-		{"label":"Positivo","data":122,"color":"#0eea57"},
-		{"label":"Neutral","data":88,"color":"#666666"},
-		{"label":"Negativo","data":214,"color":"#f44242"}],
-	"Moreno":[
-		{"label":"Positivo","data":406,"color":"#0eea57"},
-		{"label":"Neutral","data":500,"color":"#666666"},
-		{"label":"Negativo","data":500,"color":"#f44242"}],
-	"Viteri":[
-		{"label":"Positivo","data":365,"color":"#0eea57"},
-		{"label":"Neutral","data":200,"color":"#666666"},
-		{"label":"Negativo","data":100,"color":"#f44242"}],
-	"Zuquilanda":[
-		{"label":"Positivo","data":1,"color":"#0eea57"},
-		{"label":"Neutral","data":0,"color":"#666666"},
-		{"label":"Negativo","data":4,"color":"#f44242"}],
-	"Info":{"RecordCount":6,"MaxCount":1406,"Candidatos":{"Bucaram":104,"Lasso":502,"Moncayo":424,"Moreno":1406,"Viteri":665,"Zuquilanda":5}}}
+/*var testValue={"Bucaram":[
+{"label":"Positivo","data":22,"color":"#0eea57"},
+{"label":"Neutral","data":12,"color":"#666666"},
+{"label":"Negativo","data":80,"color":"#f44242"}],
+"Lasso":[
+{"label":"Positivo","data":225,"color":"#0eea57"},
+{"label":"Neutral","data":168,"color":"#666666"},
+{"label":"Negativo","data":109,"color":"#f44242"}],
+"Moncayo":[
+{"label":"Positivo","data":122,"color":"#0eea57"},
+{"label":"Neutral","data":88,"color":"#666666"},
+{"label":"Negativo","data":214,"color":"#f44242"}],
+"Moreno":[
+{"label":"Positivo","data":406,"color":"#0eea57"},
+{"label":"Neutral","data":500,"color":"#666666"},
+{"label":"Negativo","data":500,"color":"#f44242"}],
+"Viteri":[
+{"label":"Positivo","data":365,"color":"#0eea57"},
+{"label":"Neutral","data":200,"color":"#666666"},
+{"label":"Negativo","data":100,"color":"#f44242"}],
+"Zuquilanda":[
+{"label":"Positivo","data":1,"color":"#0eea57"},
+{"label":"Neutral","data":0,"color":"#666666"},
+{"label":"Negativo","data":4,"color":"#f44242"}],
+"Info":{"RecordCount":6,"MaxCount":1406,"Candidatos":{"Bucaram":104,"Lasso":502,"Moncayo":424,"Moreno":1406,"Viteri":665,"Zuquilanda":5}}}
+*/
 
 //instance DashDB driver
 var ibmdb = require("ibm_db");
 //TODO: Read from environment variable
 var dbConnString = "DATABASE=BLUDB;HOSTNAME=dashdb-entry-yp-dal09-10.services.dal.bluemix.net;PORT=50001;PROTOCOL=TCPIP;UID=dash13400;PWD=74b951a71274;Security=SSL;";
 var Pool = ibmdb.Pool,
-	pool = new Pool(),
-	cn = dbConnString,
-	qryTweetCount = "SELECT ELECCIONES.CANDIDATO," +
-	"SUM(CASE ELECCIONES.SENTIMIENTO WHEN 'POSITIVE' THEN 1 ELSE 0 END) AS POSITIVOS,\n" +
-	"SUM(CASE ELECCIONES.SENTIMIENTO WHEN 'NEUTRAL'  THEN 1 ELSE 0 END) AS NEUTRAL,\n" +
-	"SUM(CASE ELECCIONES.SENTIMIENTO WHEN 'NEGATIVE' THEN 1 ELSE 0 END) AS NEGATIVOS,\n" +
-	"COUNT(*) AS TOTAL\n" +
-	"FROM ELECCIONES\n" +
-	"GROUP BY ELECCIONES.CANDIDATO;";
+pool = new Pool(),
+cn = dbConnString,
+qryTweetCount = "SELECT *  FROM DASH13400.TWEETCOUNT;" ;
 
 function arrangeTweetCount(data) {
 	var result = {},
-	  max=0,
-		candidatos={},
-		totalRegistros=0;
+	max=0,
+	candidatos={},
+	totalRegistros=0;
 	for (var k in data) {
 		result[data[k].CANDIDATO] = [ {"label": "Positivo", "data": parseInt(data[k].POSITIVOS), "color":"#0eea57" },
-		    {"label":"Neutral", "data":parseInt(data[k].NEUTRAL), "color":"#6d6d6d"},
-			{"label":"Negativo", "data":parseInt(data[k].NEGATIVOS), "color":"#f44242" }
+		{"label":"Neutral", "data":parseInt(data[k].NEUTRAL), "color":"#6d6d6d"},
+		{"label":"Negativo", "data":parseInt(data[k].NEGATIVOS), "color":"#f44242" }
 
-		];
-		candidatos[data[k].CANDIDATO]=parseInt(data[k].POSITIVOS)+parseInt(data[k].NEGATIVOS)+parseInt(data[k].NEUTRAL);
-		totalRegistros++;
+	];
+	candidatos[data[k].CANDIDATO]=parseInt(data[k].POSITIVOS)+parseInt(data[k].NEGATIVOS)+parseInt(data[k].NEUTRAL);
+	totalRegistros++;
 
-		max = candidatos[data[k].CANDIDATO] >= max ? candidatos[data[k].CANDIDATO]: max;
+	max = candidatos[data[k].CANDIDATO] >= max ? candidatos[data[k].CANDIDATO]: max;
 
-	}
-	result["Info"] = { "RecordCount":totalRegistros, "MaxCount":max, "Candidatos":candidatos };
+}
+result["Info"] = { "RecordCount":totalRegistros, "MaxCount":max, "Candidatos":candidatos };
 
-	return result;
+return result;
 }
 
 app.get("/getTweetCount", function(request, response) {
@@ -127,7 +122,7 @@ app.get("/popularTweet", function(request, response){
 	}
 	pool.open(cn, function(err_con, db){
 		if(err_con) {
-			console.error("Error getting connection from pool: ", err_con);
+			console.error("Error getting connection from pool", err_con);
 			response.status(500);
 			response.setHeader("Content-Type", "text/plain");
 			response.write(err_con.toString());
